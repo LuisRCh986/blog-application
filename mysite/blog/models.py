ecf85e9c47f204f367f128fdcb1feaf283e47742
Luis Ramos <ramoschiroqueluis3@gmail.com>
2026-06-19 06:29:46 -0500

    feature: add list and detail views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,12 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
 # Create your models here.
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -24,6 +30,8 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT,
     )
+    objects = models.Manager() # The default manager
+    published = PublishedManager() # Our custom manager
 
     class Meta:
         ordering = ['-publish']
