3fc0eb13d04ab4a773ec976a3d0ddf08af4040dc
Luis Ramos <ramoschiroqueluis3@gmail.com>
2026-06-20 13:58:51 -0500

    add canonical url

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return (
             super().get_queryset().filter(status=Post.Status.PUBLISHED)
         )
+
 
 # Create your models here.
 class Post(models.Model):
@@ -15,7 +18,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -30,8 +33,8 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT,
     )
-    objects = models.Manager() # The default manager
-    published = PublishedManager() # Our custom manager
+    objects = models.Manager()  # The default manager
+    published = PublishedManager()  # Our custom manager
 
     class Meta:
         ordering = ['-publish']
@@ -41,3 +44,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
