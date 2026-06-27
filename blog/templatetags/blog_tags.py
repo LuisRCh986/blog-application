1f8632eca6242ba6117ef11a98857f889fe39ad4
Luis Ramos <ramoschiroqueluis3@gmail.com>
2026-06-27 12:34:29 -0500

    feat: template tag most commented

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -12,3 +12,9 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def get_most_commented_post(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comment')
+    ).order_by('-total_comments')[:count]
